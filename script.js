6fbf61df98b40a0d40430dbd6c2b403f8eb9bbc8
Gunel2727 <gunelemu@code.edu.az>
2026-02-28 23:47:16 +0400

    new code

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let age=parseInt(prompt("enter age:"));
 if(age%3==0){
     console.log("3e bolunur");
 }
-if(age%3==0){
-    console.log("3e bolunur");
+if(age%7==0){
+    console.log("7e bolunur");
 }
 else{
     console.log("bolunmur");
